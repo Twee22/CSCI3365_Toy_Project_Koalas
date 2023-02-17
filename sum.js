1486e7b9c5ecbbad7fd64693f7552e614bc3f8ba
seanfitz922 <seanfitz922@gmail.com>
2023-02-16 22:51:12 -0600

    Update sum with step
    Updated sum of sequence to use a step

--- sum.js
+++ sum.js
@@ -5,23 +5,44 @@ myForm.addEventListener("submit", (e) => {
     e.preventDefault();
     var startNumber = document.getElementById("startN").value;
     var endNumber = document.getElementById("endN").value;
+    var stepNumber = document.getElementById("step").value;
     
     // check for number input
-    if (isNaN(startNumber) || isNaN(endNumber) || startNumber == "" || endNumber == "") {
+    if (isNaN(startNumber) || isNaN(endNumber) || isNaN(stepNumber) || startNumber == "" || endNumber == "" || stepNumber =="") {
         document.getElementById("dialogP").innerHTML = "Must input only numbers";
         myDialog.showModal();
         return false;
     
-        } else {
-            // call to function sumSequence
-            sumSequence(startNumber.trim(), endNumber.trim());
-           }
+        // error checking for invalid input for sum
+    } else if (stepNumber == "0") {
+        document.getElementById("dialogP").innerHTML = "Step number can not be 0";
+        myDialog.showModal();
+        return false;
+
+    } else if (startNumber < endNumber && stepNumber < 0)  {
+        document.getElementById("dialogP").innerHTML = "Invalid input";
+        myDialog.showModal();
+        return false;
+
+    } else if (startNumber > endNumber && stepNumber > 0) {
+        document.getElementById("dialogP").innerHTML = "Invalid input";
+        myDialog.showModal();
+        return false;
+
+        // call to fumction sumSequenceN with negative step
+    } else if (parseInt(stepNumber) < 0) {
+        sumSequenceN(startNumber.trim(), endNumber.trim(), stepNumber.trim());
+
+    } else {
+        // call to function sumSequence with positive step
+        sumSequence(startNumber.trim(), endNumber.trim(), stepNumber.trim());
+        }
 })
 
-// function to output sum of numbers between startNumber and endNumber
-function sumSequence(startNumber, endNumber) {
+// function to output sum of numbers between startNumber and endNumber with positive step
+function sumSequence(startNumber, endNumber, stepNumber) {
     var sum = 0;
-    for (i = parseInt(startNumber); i <= parseInt(endNumber); i++ ) {
+    for (i = parseInt(startNumber); i <= parseInt(endNumber); i+= parseInt(stepNumber)) {
         sum+= i;
     }
     // output sum to dialog
@@ -30,4 +51,15 @@ function sumSequence(startNumber, endNumber) {
     return 0;
 }
 
+// function to output sum of numbers between startNumber and endNumber with negative step
+function sumSequenceN(startNumber, endNumber, stepNumber) {
+    var sum = 0;
+    for (i = parseInt(startNumber); i >= parseInt(endNumber); i+= parseInt(stepNumber)) {
+        sum+= i;
+    }
+    // output sum to dialog
+    document.getElementById("dialogP").innerHTML = "The sequential sum of " + startNumber + " and " + endNumber + " is " + sum; 
+        myDialog.showModal();
+    return 0;
+}
 
